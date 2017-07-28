35e0a00e1de3b5f25e976819c23a5080b69b3e92
Danielle Westerman <daniellepwesterman@gmail.com>
2017-07-28 14:58:24 -0400

    title animation

--- browser/index.js
+++ browser/index.js
@@ -1,9 +1,6 @@
 import scss from '../index.scss';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import axios from 'axios'
 import AppContainer from './containers/AppContainer';
 
 ReactDOM.render(
