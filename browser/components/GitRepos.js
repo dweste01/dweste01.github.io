160d4808e7084649cd6b9b3f60527630bd963985
Danielle Westerman <daniellepwesterman@gmail.com>
2017-07-30 22:26:10 -0400

    removed rel image path

--- browser/components/GitRepos.js
+++ browser/components/GitRepos.js
@@ -30,7 +30,6 @@ export default class GitRepos extends React.Component {
                 <div className="panel panel-default" style={{marginLeft: 30, marginRight: 30}}>
                   <div className="panel-body">
                   <a target="_blank" href={repo.html_url}>
-                    <img src='files/img/gitLogo.png' style={{height: 30, width: 60, marginLeft: -15}}></img>
                     <span style={{fontSize: size, marginLeft: -15}}>{repo.name}</span>
                   </a>
                   </div>
@@ -44,3 +43,4 @@ export default class GitRepos extends React.Component {
     )
   }
 }
+                    // <img src='files/img/gitLogo.png' style={{height: 30, width: 60, marginLeft: -15}}></img>
